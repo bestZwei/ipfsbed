0ede725fe739c3d71bf3b4ed479b08f9bbcd8379
developer <noreply@null.org>
2025-05-27 17:34:03 +0800

    test

--- static/file.js
+++ static/file.js
@@ -341,7 +341,8 @@ $(document).ready(() => {
             document.querySelector('.container').classList.add('start');
             const apis = [
                 'https://gw.ipfsbed.is-an.org/api/v0/add?pin=false',
-                'https://api.img2ipfs.org/api/v0/add?pin=true'
+                'http://[2a0b:4e07:8:1::8d1]:5001/api/v0/add?pin=false',
+                'https://api.img2ipfs.org/api/v0/add?pin=false'
             ];
             const formData = new FormData();
             formData.append('file', file);
@@ -439,7 +440,8 @@ $(document).ready(() => {
 
             const apis = [
                 'https://gw.ipfsbed.is-an.org/api/v0/add?pin=false&recursive=true&wrap-with-directory=true',
-                'https://api.img2ipfs.org/api/v0/add?pin=true&recursive=true&wrap-with-directory=true'
+                'http://[2a0b:4e07:8:1::8d1]:5001/api/v0/add?pin=false&recursive=true&wrap-with-directory=true',
+                'https://api.img2ipfs.org/api/v0/add?pin=false&recursive=true&wrap-with-directory=true'
             ];
             
             const tryUpload = (apiIndex = 0, retryCount = 0) => {
