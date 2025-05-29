bdcc579b9de7faca5fb1a691f1a7e0ba2ef8f270
developer <noreply@null.org>
2025-05-29 20:16:00 +0800

    Update API endpoint for file uploads to use a secure HTTPS connection

--- static/file.js
+++ static/file.js
@@ -403,7 +403,7 @@ $(document).ready(() => {
             document.querySelector('.container').classList.add('start');
             const apis = [
                 'https://gw.ipfsbed.is-an.org/api/v0/add?pin=false',
-                'http://[2a0b:4e07:8:1::8d1]:5001/api/v0/add?pin=false'
+                'https://gw2.ipfsbed.is-an.org/api/v0/add?pin=false'
                 // 'https://api.img2ipfs.org/api/v0/add?pin=false'
             ];
             const formData = new FormData();
